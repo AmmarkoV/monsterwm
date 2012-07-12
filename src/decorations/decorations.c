bc468e241a5029ba0df482be924289776f5643b3
Ammar Qammaz <ammarkov@gmail.com>
2012-07-13 02:54:03 +0300

    ...

--- src/decorations/decorations.c
+++ src/decorations/decorations.c
@@ -1,13 +1,13 @@
-// Written by Ch. Tronche (http://tronche.lri.fr:8000/)
-#include <X11/Xlib.h> // Every Xlib program must include this
-#include <assert.h>   // I include this to test return values the lazy way
+#include <X11/Xlib.h>
 
-#define FOCUSED_WINDOW   0x01
-#define HIGHLIGHT_MINIMIZE 0x02
-#define HIGHLIGHT_MAXIMIZE 0x04
-#define HIGHLIGHT_CLOSE    0x08
-#define OVER_BUTTON     0x16
-#define ACTIVE_BUTTON  0x32
+#define UNUSABLE_BYTE      1
+#define UNUSABLE_BYTE2     2
+#define FOCUSED_WINDOW     4
+#define HIGHLIGHT_MINIMIZE 8
+#define HIGHLIGHT_MAXIMIZE 16
+#define HIGHLIGHT_CLOSE    32
+#define OVER_BUTTON        64
+#define ACTIVE_BUTTON      128
 
 const unsigned long    overColor  = 0x000011aa;
 const unsigned long    selectColor  = 0x00ff1100;
@@ -22,7 +22,7 @@ const unsigned long    barInactiveColor2 = 0x00999999;
 const unsigned long    barInactiveColor3 = 0x00aaaaaa;
 
 
-void drawWindowBar(unsigned int x,unsigned int y,unsigned int barWidth,unsigned int barHeight,unsigned int settings,char * message,Display *dpy)
+void drawWindowBar(unsigned int x,unsigned int y,unsigned int barWidth,unsigned int barHeight,unsigned short settings,char * message,Display *dpy)
 {
       unsigned int barHeightMiddle = (unsigned int ) barHeight/2;
 
@@ -34,7 +34,7 @@ void drawWindowBar(unsigned int x,unsigned int y,unsigned int barWidth,unsigned 
                                    { barBackColor=&barInactiveColor; barColor1=&barInactiveColor1; barColor2=&barInactiveColor2; barColor3=&barInactiveColor3; }
 
       // Create the window
-      Window w = XCreateSimpleWindow(dpy, DefaultRootWindow(dpy), 0, 0, barWidth , barHeight , 0 , *barBackColor , *barBackColor);
+      Window w = XCreateSimpleWindow(dpy, DefaultRootWindow(dpy), x,y, barWidth,barHeight,0, *barBackColor , *barBackColor);
 
       // We want to get MapNotify events
       XSelectInput(dpy, w, StructureNotifyMask);
@@ -73,35 +73,31 @@ void drawWindowBar(unsigned int x,unsigned int y,unsigned int barWidth,unsigned 
 	       //We want thin lines
            XSetLineAttributes(dpy,gc,1,0,0,0);
 
-           if ( (settings&ACTIVE_BUTTON) || (settings&OVER_BUTTON) )
-           { //Minimize , Maximize or Close button are active , drawing them
-            if (settings&OVER_BUTTON) { XSetForeground(dpy, gc, overColor); } else
-            if (settings&ACTIVE_BUTTON) { XSetForeground(dpy, gc, selectColor); }
+           //fprintf(stderr,"%u&%u=%u , %u&%u=%u \n",settings,HIGHLIGHT_MINIMIZE,settings&HIGHLIGHT_MINIMIZE,settings,HIGHLIGHT_MAXIMIZE,settings&HIGHLIGHT_MAXIMIZE);
 
-            if (settings&HIGHLIGHT_MINIMIZE) {  XFillRectangle(dpy,w,gc,barWidth-45,1,15,barHeight-2); } else
-            if (settings&HIGHLIGHT_MAXIMIZE) {  XFillRectangle(dpy,w,gc,barWidth-30,1,15,barHeight-2); } else
-            if (settings&HIGHLIGHT_CLOSE)    {  XFillRectangle(dpy,w,gc,barWidth-15,1,15,barHeight-2); }
+           if ( ((settings&ACTIVE_BUTTON)==ACTIVE_BUTTON) || ((settings&OVER_BUTTON)==OVER_BUTTON ) )
+           { //Minimize , Maximize or Close button are activated or hovered over , drawing them
+            if ((settings&OVER_BUTTON)==OVER_BUTTON) { XSetForeground(dpy, gc, overColor); } else
+            if ((settings&ACTIVE_BUTTON)==ACTIVE_BUTTON) { XSetForeground(dpy, gc, selectColor); }
+
+            if ((settings&HIGHLIGHT_MINIMIZE)==HIGHLIGHT_MINIMIZE) {  XFillRectangle(dpy,w,gc,barWidth-45,1,15,barHeight-2); } else
+            if ((settings&HIGHLIGHT_MAXIMIZE)==HIGHLIGHT_MAXIMIZE) {  XFillRectangle(dpy,w,gc,barWidth-30,1,15,barHeight-2); } else
+            if ((settings&HIGHLIGHT_CLOSE)==HIGHLIGHT_CLOSE)       {  XFillRectangle(dpy,w,gc,barWidth-15,1,15,barHeight-2); }
            }
 
            // Tell the GC we draw using the white color
            XSetForeground(dpy, gc, blackColor);
 
            XSetLineAttributes(dpy,gc,3,0,0,0);
-           /*X*/
-           XDrawLine(dpy, w, gc, barWidth - 10, 6 , barWidth - 4 , barHeight-6);
-           XDrawLine(dpy, w, gc, barWidth - 10, barHeight-6 , barWidth - 4 , 6);
-
-           /*Maximize*/
-           XDrawRectangle(dpy, w ,  gc, barWidth - 25, 7 , 5, barHeight-14 );
-
-           /*Minimize*/
-           XDrawLine(dpy, w ,  gc, barWidth - 40, barHeightMiddle , barWidth - 35 , barHeightMiddle );
+           /*Minimize Icon*/ XDrawLine(dpy, w ,  gc, barWidth - 40, barHeightMiddle , barWidth - 35 , barHeightMiddle );
+           /*Maximize Icon*/ XDrawRectangle(dpy, w ,  gc, barWidth - 25, 7 , 5, barHeight-14 );
+           /*X*/ XDrawLine(dpy, w, gc, barWidth - 10, 6 , barWidth - 4 , barHeight-6);
+                 XDrawLine(dpy, w, gc, barWidth - 10, barHeight-6 , barWidth - 4 , 6);
 
 
-
-           if (!(settings&FOCUSED_WINDOW))  XSetForeground(dpy, gc, whiteColor); // We are inactive so we want a white font due to the dark background
+           if ((settings&FOCUSED_WINDOW)!=FOCUSED_WINDOW)  XSetForeground(dpy, gc, whiteColor); // We are inactive so we want a white font due to the dark background
            /* Title of Window */
-           XDrawString(dpy,w,gc, 40,15, "Title of window goes here",25); //todo add handler for large strings that appends ...
+           XDrawString(dpy,w,gc, 40,15, "Title of window goes here",25); //todo add handler for large strings that appends ... and centers text
 
            //Send the requests to the server
            XFlush(dpy);
@@ -115,9 +111,10 @@ main()
 {
       // Open the display
       Display *dpy = XOpenDisplay(0);
-      assert(dpy);
+      if (!dpy ) return 1;
 
-      drawWindowBar(0,0,300,20,FOCUSED_WINDOW|HIGHLIGHT_MINIMIZE,"Title of window goes here",dpy);
+      drawWindowBar(0,0,400,20,FOCUSED_WINDOW|HIGHLIGHT_CLOSE|ACTIVE_BUTTON,"Title of window goes here",dpy);
+     // drawWindowBar(0,20,400,20,FOCUSED_WINDOW|HIGHLIGHT_MINIMIZE,"Title of window goes here",dpy);
 
       return 0;
 }
